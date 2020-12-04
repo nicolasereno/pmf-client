425f54b6d233f751c3ae46168bfbd42babb9be27
nicola.sereno <nicola.sereno@HW002134.lynx.italy>
2020-12-04 16:45:23 +0100

    URL del backend di produzione reso relativo
    L'URL del backend di produzione adesso presuppone che i files della GUI
    e i servizi del backend siano dietro lo stesso web server: in questo
    modo lo stesso compilato può essere utilizzato sia per sviluppo che per
    collaudo.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
 	production: true,
-	baseUrl: 'http://c6aawipalf03/ext/pmfController/v1/'
+	baseUrl: '/../ext/pmfController/v1/',
+//	baseUrl: 'http://s6aawipalf00/ext/pmfController/v1/'
+//	baseUrl: 'http://c6aawipalf03/ext/pmfController/v1/'
 };
